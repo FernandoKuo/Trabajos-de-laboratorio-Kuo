fcc6acb923b0c3eda71a5a22ec616811dfd45d2e
FernandoKuo <101562764+FernandoKuo@users.noreply.github.com>
2022-11-14 17:31:56 -0300

    Delete Trabajos en archivos Kuo directory

--- Trabajos en archivos Kuo/Cadenas txt Kuo/Cadenas txt 1 Kuo/Cadenas txt 1 Kuo.py
+++
@@ -1,9 +0,0 @@
-
-oracion = input('Ingrese una oracion: ')
-esp_cant = len(oracion.split()) - 1
-
-#transferencia
-data = open('Cantidad de espacios.txt', 'w')
-data.write(f'La ultima oracion ingresada tenia {esp_cant} espacios\n')
-data.write(f'La oracion: "{oracion}"\n')
-data.close()
