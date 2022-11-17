e77fdffad014a6fec6da9ecba7128a388a017c39
FernandoKuo <101562764+FernandoKuo@users.noreply.github.com>
2022-11-17 20:56:28 -0300

    Add files via upload

---
+++ Flags Guess Kuo Hossein/FG_data.py
@@ -0,0 +1,17 @@
+import random
+
+#datos necesarios para hacer el juego
+#puestos aca para ahorrar espacio
+
+#amongus
+amowords = ['R.D.C. es Rupública Democrática del Congo', 'I Found Among Us', 'Creditos a Tonanyulo3', 'Un poco SUS no crees?', 'Sabias que el pais mas chico del mundo es Vaticano?', 'Australia es mas ancha que la luna', 'Juego es inspirado por el World Flag Quiz', 'Vamos! tu puedes!', 'Te reto a sacar la nota perfecta', 'Estoy caminando!', 'Que pasará si me presionas?', '.-.', '', '']
+
+#nombres de los paises de las banderas
+#R.D.C. means república democrútica del congo
+countries = ['Afganistán', 'Alemania', 'Angola', 'Arabia Saudí', 'Argelia', 'Argentina', 'Australia', 'Bangladesh', 'Brasil', 'Burkina Faso', 'Camboya', 'Camerún', 'Canadá', 'Chad', 'Chile', 'China', 'Colombia', 'Corea del Norte', 'Corea del Sur', 'Costa de Marfil', 'Ecuador', 'Egipto', 'España', 'Estados Unidos', 'Etiopía', 'Filipinas', 'Francia', 'Ghana', 'Guatemala', 'Guinea', 'India', 'Indonesia', 'Irak', 'Irán', 'Italia', 'Japón', 'Kazajistán', 'Kenia', 'Madagascar', 'Malasia', 'Malawi', 'Malí', 'Marruecos', 'Mozambique', 'Myanmar', 'México', 'Nepal', 'Nigeria', 'Níger', 'Pakistán', 'Países Bajos', 'Perú', 'Polonia', 'Reino Unido', 'R.D.C.', 'Rumanía', 'Rusia', 'Senegal', 'Siria', 'Somalia', 'Sri Lanka', 'Sudáfrica', 'Sudán', 'Tailandia', 'Taiwán', 'Tanzania', 'Turquía', 'Ucrania', 'Uganda', 'Uzbekistán', 'Venezuela', 'Vietnam', 'Yemen', 'Zambia', 'Zimbabue']
+test_countries = random.sample(countries, 5)
+
+#24 provincias
+#porque buenos aires se repite?
+provinces = ['Buenos Aires', 'C.A.B.A.', 'Catamarca', 'Chaco', 'Chubut', 'Córdoba', 'Corrientes', 'Entre Ríos', 'Formosa', 'Jujuy', 'La Pampa', 'La Rioja', 'Mendoza', 'Misiones', 'Neuquén', 'Río Negro', 'Salta', 'San Juan', 'San Luis', 'Santa Cruz', 'Santa Fe', 'Santiago del Estero', 'Tierra del Fuego', 'Tucumán']
+test_provinces = random.sample(provinces, 5)
